29c35eeb43b34e5c2a42308bd61f4cabaca85d62
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-12 00:47:27 -0700

    removed the margin from the top of the h1 to get rid of the white strip showing up

--- src/ColorPage.js
+++ src/ColorPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import './ColorPage.css';
 
 const ColorPage = ({ colorsArr }) => {
   const { color: colorName } = useParams();
   const [{ value: colorVal }] = colorsArr.filter(colorObj => colorObj.name === colorName);
 
   return (
-    <div style={{backgroundColor: colorVal}}>
+    <div className="ColorPage" style={{backgroundColor: colorVal}}>
       <h1>This is the {colorName} page!</h1>
       <h3><Link to="/colors">Go Back</Link></h3>
     </div>
